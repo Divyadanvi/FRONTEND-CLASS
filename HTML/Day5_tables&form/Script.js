4e3a14d6434d83e94ba4ebea63ff174affa36387
Divyadanvi <d.divya10003@gmail.com>
2025-01-04 19:56:39 +0530

    dfee

---
+++ HTML/Day5_tables&form/Script.js
@@ -0,0 +1,18 @@
+<script>
+const form = document.getElementById("my form");
+const ErrorMessage = document.getElementById("ErrorMessage");
+
+form.addEventListener('submit', function(event){
+ErrorMessage.textcontent="";
+
+const Password = document.getElementById("Password").value;
+if(Password.length < 6)
+{
+    Event.preventDefault();
+    ErrorMessage.textcontent = "Password"
+}
+
+
+})
+
+</script>
